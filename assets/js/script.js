9578b03dbcbe5f12ab0824ddff827e997c074b39
Juanjohn11 <elmatanaco1144@gmail.com>
2021-12-01 18:00:34 -0800

    adding more functions to grab data adding a save history ffunction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,63 @@
 var apiKey= "32eb3d91e45e32ef2ed78d64e217de54"
 
-function getWeather (){
-    var cityName = "sacramento"
+
+function saveHis (name){
+    var searchHistory = JSON.parse(localStorage.getItem('searchhis'))|| []
+    searchHistory.push(name)
+    localStorage.setItem('searchhis',JSON.stringify(searchHistory))
+    //displayHistory()
+
+}
+function displayHistory(){
+    var searchHistory = JSON.parse(localStorage.getItem('searchhis'))|| []
+    for (var i = 0; i < searchHistory.length; i++){
+        var item = searchHistory[i]
+        var hist = document.createElement("p");   
+            hist.textContent = item;                 
+            document.getElementById('history').appendChild(hist);
+
+    }
+
+
+}
+
+function getWeather (event){
+    event.preventDefault()
+    var cityName = document.getElementById('cityLoc').value;
     var url = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&units=imperial&appid=" + apiKey
 
     fetch(url)
     .then(function(res){
         return res.json()
     })
-    .then(function(data){
-        console.log(data);    
-        
-        var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat + "&lon="+data.coord.lon + "&units=imperial&appid=" + apiKey
+    .then(function(res){
+        console.log(res);    
+        saveHis(res.name)
+        var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat="+res.coord.lat + "&lon="+res.coord.lon + "&units=imperial&appid=" + apiKey
 
         fetch(url2)
         .then(function(res){
             return res.json()
         })
         .then(function(data){
+            var output = document.getElementById('output')
+            output.innerHTML= "";
             console.log(data)
+            var btn = document.createElement("p");   
+            btn.textContent = "City " + res.name;                 
+            output.appendChild(btn);
+           var btn = document.createElement("p");   
+            btn.textContent = "Temp " + data.current.temp;                 
+            output.appendChild(btn);
+
+            var btn = document.createElement("p");   
+            btn.textContent = "hum " + data.current.humidity;                 
+            output.appendChild(btn);
         })
 
     })
-    
 }
-getWeather();
+document.getElementById('user-form').addEventListener('submit',getWeather);
+
+displayHistory()
+
