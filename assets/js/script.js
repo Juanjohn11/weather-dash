5990c58527ca833b4f382c88dc30d5214348f0d5
Juanjohn11 <elmatanaco1144@gmail.com>
2021-12-03 07:54:17 -0800

    added to a 5 day forcast added more col to my html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ function saveHis (name){
     var searchHistory = JSON.parse(localStorage.getItem('searchhis'))|| []
     searchHistory.push(name)
     localStorage.setItem('searchhis',JSON.stringify(searchHistory))
-    //displayHistory()
 
 }
 function displayHistory(){
@@ -32,7 +31,7 @@ function getWeather (event){
     })
     .then(function(res){
         console.log(res);    
-        saveHis(res.name)
+        //saveHis(res.name)
         var url2 = "https://api.openweathermap.org/data/2.5/onecall?lat="+res.coord.lat + "&lon="+res.coord.lon + "&units=imperial&appid=" + apiKey
 
         fetch(url2)
@@ -40,19 +39,141 @@ function getWeather (event){
             return res.json()
         })
         .then(function(data){
+            
             var output = document.getElementById('output')
             output.innerHTML= "";
             console.log(data)
-            var btn = document.createElement("p");   
+            
+            var btn = document.createElement("h1");   
             btn.textContent = "City " + res.name;                 
             output.appendChild(btn);
-           var btn = document.createElement("p");   
-            btn.textContent = "Temp " + data.current.temp;                 
+            
+            var btn = document.createElement("p");   
+            btn.textContent = "Temp: " + data.current.temp + "F";                 
             output.appendChild(btn);
 
             var btn = document.createElement("p");   
-            btn.textContent = "hum " + data.current.humidity;                 
+            btn.textContent = "Humidity: " + data.current.humidity + "%";                 
             output.appendChild(btn);
+
+            var btn = document.createElement("p");   
+            btn.textContent = "UV Index: " + data.current.uvi;                 
+            output.appendChild(btn);
+
+            var btn = document.createElement("p");   
+            btn.textContent = "Wind: " + res.wind.speed + " MPH";                 
+            output.appendChild(btn);
+
+            //5 day 
+
+            var output1 = document.getElementById('out1')
+            output1.innerHTML = "";
+
+            // var btn = document.createElement("h1");
+            // btn.textContent = "5 Day forcast "
+            // output1.appendChild(btn);
+            //the next day [1]
+            var btn = document.createElement("p");
+            btn.textContent = "Temp: " + data.daily[1].temp.max;
+            output1.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "Humidity: " + data.daily[1].humidity;
+            output1.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "UV Index: " + data.daily[1].uvi;
+            output1.appendChild(btn);
+            
+            var btn = document.createElement("p");
+            btn.textContent = "Wind: " + data.daily[1].wind_speed;
+            output1.appendChild(btn);
+
+            //[2]
+            var output2 = document.getElementById('out2')
+            output2.innerHTML= "";
+
+
+            var btn = document.createElement("p");
+            btn.textContent = "Temp: " + data.daily[2].temp.max;
+            output2.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "Humidity: " + data.daily[2].humidity;
+            output2.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "UV Index: " + data.daily[2].uvi;
+            output2.appendChild(btn);
+            
+            var btn = document.createElement("p");
+            btn.textContent = "Wind: " + data.daily[2].wind_speed;
+            output2.appendChild(btn);
+
+            //[3]
+
+            var output3 = document.getElementById('out3')
+            output3.innerHTML="";
+
+            var btn = document.createElement("p");
+            btn.textContent = "Temp: " + data.daily[3].temp.max;
+            output3.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "Humidity: " + data.daily[3].humidity;
+            output3.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "UV Index: " + data.daily[3].uvi;
+            output3.appendChild(btn);
+            
+            var btn = document.createElement("p");
+            btn.textContent = "Wind: " + data.daily[3].wind_speed;
+            output3.appendChild(btn);
+
+            //[4]
+
+            var output4 = document.getElementById('out4')
+            output4.innerHTML="";
+
+            var btn = document.createElement("p");
+            btn.textContent = "Temp: " + data.daily[4].temp.max;
+            output4.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "Humidity: " + data.daily[4].humidity;
+            output4.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "UV Index: " + data.daily[4].uvi;
+            output4.appendChild(btn);
+            
+            var btn = document.createElement("p");
+            btn.textContent = "Wind: " + data.daily[4].wind_speed;
+            output4.appendChild(btn);
+
+            //[5]
+
+            var output5 = document.getElementById('out5')
+            output5.innerHTML="";
+
+            var btn = document.createElement("p");
+            btn.textContent = "Temp: " + data.daily[5].temp.max;
+            output5.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "Humidity: " + data.daily[5].humidity;
+            output5.appendChild(btn);
+
+            var btn = document.createElement("p");
+            btn.textContent = "UV Index: " + data.daily[5].uvi;
+            output5.appendChild(btn);
+            
+            var btn = document.createElement("p");
+            btn.textContent = "Wind: " + data.daily[5].wind_speed;
+            output5.appendChild(btn);
+            
+
         })
 
     })
